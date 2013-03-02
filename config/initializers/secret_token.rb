b2d9981a148b020a7f035daf8e7dd6190407d333
Libby Kusuma <libby.kusuma@chictopia.com>
2013-03-01 21:53:38 -0800

    Created rails application, status, and mood updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibbyBook::Application.config.secret_token = 'fbab6a4172b663c94535cb9ab6a4eebaacd44776ecab4586d8e31b291a87b39cafd757a83a0db5a36d70610c202ee25ff8e8dbecc27a850e5fc433878e35eca8'
